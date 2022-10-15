01260e040f91d926fba08f12d877d367b81123f0
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2022-10-15 17:11:58 +0300

    Update install_proxy_with_auth.sh

--- install_proxy_with_auth.sh
+++ install_proxy_with_auth.sh
@@ -113,7 +113,7 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=11150
-LAST_PORT=11650
+LAST_PORT=11475
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh

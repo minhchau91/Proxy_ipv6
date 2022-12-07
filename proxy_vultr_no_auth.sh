8573a7aee079722bee5b5fac13139b8a0d0793ec
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2022-12-07 17:53:24 +0300

    Update proxy_vultr_no_auth.sh

--- proxy_vultr_no_auth.sh
+++ proxy_vultr_no_auth.sh
@@ -112,8 +112,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=11111
-LAST_PORT=11410
+FIRST_PORT=40000
+LAST_PORT=40299
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh

20ef2fb7fcd256bfca51c5f21dda79064f2d4b2c
minhchau91 <96756121+minhchau91@users.noreply.github.com>
2022-12-22 11:40:22 +0300

    Update Rotation.sh

--- Rotation.sh
+++ Rotation.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 random() {
-	tr </dev/urandom -dc A-Za-z0-9 | head -c5
-	echo
+        tr </dev/urandom -dc A-Za-z0-9 | head -c5
+        echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-	ip64() {
-		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-	}
-	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+        ip64() {
+                echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+        }
+        echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
 gen_3proxy() {
@@ -24,40 +24,41 @@ nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6291456 
+stacksize 6291456
 flush
 auth none
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 $(awk -F "/" '{print "auth none\n" \
 "allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"proxy -6 -n -a -p" $5 " -i" $4 " -e"$6"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "mcdn/huladeca112/$IP4/$port/$(gen64 $IP6)"
+        echo "mcdn/huladeca112/$interface/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
 gen_iptables() {
     cat <<EOF
-    $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+    $(awk -F "/" '{print "/sbin/iptables -I INPUT -p tcp --dport " $5 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
 }
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig enp1s0 inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "/sbin/ifconfig " $3 " inet6 add " $6 "/64"}' ${WORKDATA})
 EOF
 }
 /sbin/service network restart
+#/sbin/iptables -F INPUT
 echo "installing apps"
-rm -f /usr/local/etc/3proxy/3proxy.cfg
-rm -f /home/proxy-installer/data.txt
-rm -f /home/proxy-installer/boot_iptables.sh
-rm -f /home/proxy-installer/boot_ifconfig.sh
+rm -fv /usr/local/etc/3proxy/3proxy.cfg
+rm -fv /home/proxy-installer/data.txt
+rm -fv /home/proxy-installer/boot_iptables.sh
+rm -fv /home/proxy-installer/boot_ifconfig.sh
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
@@ -65,21 +66,28 @@ WORKDATA="${WORKDIR}/data.txt"
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-
+interface=$(ip addr show | awk '/inet.*brd/{print $NF}')
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=40000
-LAST_PORT=40399
+LAST_PORT=40499
 
 gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
+#gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
+rm -fv /etc/rc.local
+
+cat >>/etc/rc.local <<EOF
+touch /var/lock/subsys/local
+systemctl start NetworkManager.service
+ifup ${interface}
+bash ${WORKDIR}/boot_ifconfig.sh
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg &
+EOF
+
 bash /etc/rc.local
-systemctl daemon-reload
-systemctl restart NetworkManager.service
-ifup enp1s0
-systemctl restart 3proxy.service
+/sbin/reboot
